9a20b99f1f53d20d29444b34b21350e731d81306
Fedor Nemtsev <fnemtsev@inscyth.com>
2022-03-13 16:05:00 +0200

    задание 11

--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -8,9 +8,8 @@ namespace WebAddressbookTests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
-        //private string name;
-        //private string lastName;
-
+        private string allPhones;
+        private string allEmails;
 
         public ContactData(string name)
         {
@@ -25,6 +24,64 @@ namespace WebAddressbookTests
 
         public string Name { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string SecondaryPhone { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+
+        public string AllEmails
+        {
+            get
+            {
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+                else
+                {
+                    return (CleanUp(Email) + CleanUp(Email2) + CleanUp(Email3)).Trim();
+                }
+            }
+
+            set
+            {
+                allEmails = value;
+            }
+        }
+
+        public string AllPhones
+        {
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone) + CleanUp(SecondaryPhone)).Trim();
+                }
+            }
+
+            set
+            {
+                allPhones = value;
+            }
+        }
+
+        private string CleanUp(string phone)
+        {
+            if (phone == null || phone == "")
+            {
+                return "";
+            }
+            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
+
         public string Id { get; set; }
 
         public int CompareTo(ContactData other)
@@ -46,7 +103,7 @@ namespace WebAddressbookTests
             {
                 return true;
             }
-            return (Name == other.Name)&&(LastName == other.LastName);
+            return (Name == other.Name) && (LastName == other.LastName);
             //return Name == other.Name;
         }
 
